c7d62e2ed5f1b0130bd0dac853858296d0092d61
Mhayk Whandson <hi@mhayk.com>
2021-06-04 10:00:06 +0100

    The css styles to use swiper has been imported.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,12 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 
+// Import Swiper styles
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css"
+import "swiper/components/pagination/pagination.min.css"
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
